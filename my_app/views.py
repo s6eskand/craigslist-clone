43b56fe3ad541b876cdec8b91164370ce88ee911
Sam <sam.eskandar00@hotmail.com>
2020-01-03 03:37:48 -0500

    added backend functionality

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 import requests
+from requests.compat import quote_plus
 from bs4 import BeautifulSoup
+from . import models
+
+BASE_CRAIGSLIST_URL = 'https://toronto.craigslist.org/search/?query={}'
 
 
 # Create your views here.
@@ -10,6 +14,12 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class': 'result-title'})
     stuff_for_frontend = {
         'search': search,
     }
