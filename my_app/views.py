0934552ca74c13b95d7662babc42556cb936a705
Sam <sam.eskandar00@hotmail.com>
2020-01-03 02:51:40 -0500

    added 'new search' view

--- my_app/views.py
+++ my_app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(
+        request,
+        'my_app/new_search.html',
+        stuff_for_frontend,
+    )
