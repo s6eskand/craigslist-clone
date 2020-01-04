44ef4ded7f5f5e41477ee65f82ddccdefb29c3ab
Sam <sam.eskandar00@hotmail.com>
2020-01-04 14:33:57 -0500

    removed 'Price' module

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Search, Price
+from .models import Search
 
 
 # Register your models here.
 admin.site.register(Search)
-admin.site.register(Price)
