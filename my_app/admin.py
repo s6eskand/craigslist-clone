729afac175add95b6527da20e6c2c10f698023d1
Sam <sam.eskandar00@hotmail.com>
2020-01-04 14:01:20 -0500

    changes added

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Search
+from .models import Search, Price
 
 
 # Register your models here.
 admin.site.register(Search)
+admin.site.register(Price)
