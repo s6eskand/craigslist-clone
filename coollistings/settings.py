c73b7d5678ab6a878d580bb2e3799fba8f3a373f
Sam <sam.eskandar00@hotmail.com>
2020-01-06 00:33:17 -0500

    updated settings

--- coollistings/settings.py
+++ coollistings/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '$unmt7po!a93)yp*eq!t3+wnscq7wo&@@n)b=nd^q&4*h0ggy*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
