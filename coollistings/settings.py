9c8970f4a77a95b3205284f8d20f16afaa78cf22
Sam Eskandar <53276111+s6eskand@users.noreply.github.com>
2020-01-11 03:49:07 -0500

    hiding secret_key

--- coollistings/settings.py
+++ coollistings/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$unmt7po!a93)yp*eq!t3+wnscq7wo&@@n)b=nd^q&4*h0ggy*'
+SECRET_KEY = '*********************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
